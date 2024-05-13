7bc375acaeee430bcd117ecd53620b8c4df02e65
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2024-05-13 10:14:32 -0400

    fixed setTitle

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function setTitle(id){
     console.log(id);
-    document.cookie="title=a;path=/"
+    
     alert(document.cookie)
     
     if(id!==""){
-        document.cookie =  "title=a" + "; path=/"
+        document.cookie =  "title=" + document.getElementById(id) + "; path=/"
     }
     if(getCookie("title")!==""){
         document.getElementById("title").innerHTML = getCookie("title");
